8f5e63be0c49621579ecd3fac827d0bbaf82739d
rami.atrouni <ramiatrouni@gmail.com>
2020-12-11 11:28:04 +0200

    Solve Exercise 1

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,73 @@
-// 🐨 you'll need to import React and ReactDOM up here
+import '@reach/dialog/styles.css'
+import Dialog from '@reach/dialog'
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import { Logo } from './components/logo'
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+function LoginForm({ onSubmit, title }) {
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
+    const doSubmit = (event) => {
+        event.preventDefault();
 
-// 🐨 use ReactDOM to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+        onSubmit({
+            username: event.target[0].value,
+            password: event.target[1].value
+        })
+    }
+    return (
+        <form onSubmit={doSubmit}>
+            <div>
+                <label>Username</label>
+                <input type="text" id="username" />
+            </div>
+            <div className="">
+                <label>Password</label>
+                <input type="password" id="password" autoComplete="on" />
+            </div>
+            <button type="submit">{title}</button>
+        </form>
+    )
+}
+
+
+function App() {
+    const modals = {
+        login: 'LOGIN',
+        register: 'REGISTER',
+        none: 'NONE'
+    };
+
+    const login = (values) => {
+        console.log(values);
+    }
+
+    const signup = (values) => {
+        console.log(values);
+    }
+
+    const [modalState, setModalState] = useState(modals.none);
+    return (
+        <div>
+            <Logo width="80" height="80" />
+            <h1>Bookshelf</h1>
+            <div>
+                <button onClick={() => setModalState(modals.login)}>Login</button>
+            </div>
+            <div>
+                <button onClick={() => setModalState(modals.register)}>Register</button>
+            </div>
+
+            <Dialog aria-label="LoginDialog" isOpen={modalState === modals.login}>
+                <button onClick={() => setModalState(modals.none)}>Close</button>
+                <LoginForm title="Login" onSubmit={login} />
+            </Dialog>
+
+            <Dialog aria-label="SignupDialog" isOpen={modalState === modals.register}>
+                <button onClick={() => setModalState(modals.none)}>Close</button>
+                <LoginForm title="Register" onSubmit={signup} />
+            </Dialog>
+
+        </div>
+    )
+}
+ReactDOM.render(<App />, document.getElementById("root"))
